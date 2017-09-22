45bb4b82600b6e819ad01d0466cf819e3ef5b263
xiaozeng <zengxiao1028@gmail.com>
2017-09-22 09:41:07 -0400

    update

--- gen_224_cifar10.py
+++ gen_224_cifar10.py
@@ -5,7 +5,7 @@ import scipy
 import os
 def main():
 
-    save_dir = './dataset/cifar10'
+    save_dir = './dataset/cifar10/test'
 
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -16,16 +16,17 @@ def main():
     for i in range(0,10):
         class_cnt_dict[str(i)] = 0
 
-    for idx,each in enumerate(x_train):
+    for idx,each in enumerate(x_test):
         resized_image = scipy.misc.imresize(each, (224, 224))
 
         # class folder
-        label = str(y_train[idx][0])
+        label = str(y_test[idx][0])
         class_folder = os.path.join(save_dir,label)
 
         os.makedirs(class_folder,exist_ok=True)
 
         # write image to disk
+        resized_image = resized_image[...,::-1]
         cv2.imwrite(os.path.join(class_folder, str(class_cnt_dict[label]) + '.jpg'),resized_image)
 
         class_cnt_dict[label] += 1
