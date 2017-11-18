a0f6b6a6aebb3dad335ac3631a7cbcd094905e2f
xiaozeng <zengxiao1028@gmail.com>
2017-11-18 11:15:50 -0500

    update

--- ResNet50.py
+++ ResNet50.py
@@ -20,7 +20,6 @@ WEIGHTS_PATH = 'https://github.com/fchollet/deep-learning-models/releases/downlo
 WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
 from keras.preprocessing import image
 
-
 class ResNet50(object):
 
     def __init__(self, config_path , trimfrom=None):
