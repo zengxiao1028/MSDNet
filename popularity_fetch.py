c94d4a2992677b52cb29e8ce90c7f63850c977ad
Xiao Zeng <zengxiao1028@gmail.com>
2017-11-24 20:28:35 -0500

    update

--- popularity_fetch.py
+++ popularity_fetch.py
@@ -1,10 +1,38 @@
 import os
 import webbrowser
 
-def main():
-    for i in range(100):
-        webbrowser.open_new_tab('http://image-net.org/synset?wnid=n03457902')
+def open_tabs(classes_list):
+
+    wnids = [each.split()[0] for each in classes_list]
+
+    ks = list(range(0,500,50))          # [0,50,100,150,200,250,300,350,400,450]
+
+    k = ks[0] # change this from 0 to 9
+
+    for i in range(k, k + 50):
+       webbrowser.open_new_tab('http://image-net.org/synset?wnid=%s' % wnids[i])
+
+
+
+
+
+def main(label_path):
+    with open(label_path) as f:
+        lines = f.readlines()
+
+    classes_list_part1 = lines[:500]
+    print('xiao handle',len(classes_list_part1))
+
+
+    classes_list_part2 = lines[500:]
+    print('biyi handle',len(classes_list_part2))
+
+
+    open_tabs(classes_list_part1)  # change classes_list_part1 to classes_list_part2
+
+
+
 
 
 if __name__ == '__main__':
-    main()
+    main('./dataset/imagenet/map_clsloc.txt')
