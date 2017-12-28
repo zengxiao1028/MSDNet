a1c785aa0c0870cf353bf4ca85125f064187807d
Xiao Zeng <zengxiao1028@gmail.com>
2017-12-28 17:22:20 -0500

    update

--- dataset/age/prepare_dataset.py
+++ dataset/age/prepare_dataset.py
@@ -2,10 +2,18 @@ import os
 from shutil import copyfile
 
 
-labeks = {'(0, 2)':'0',
-          ''(25, 32)':'}
+labels = {'(0, 2)':'0',
+          '(4, 6)': '1',
+          '(8, 13)': '2',
+          '(15, 20)': '3',
+          '(25, 32)': '4',
+          '(38, 43)': '5',
+          '(48, 53)': '6',
+          '(60, 100)': '7',}
 def main():
 
+    scr_folder = ''
+    dst_folder = ''
     with open('fold_0_data.txt') as f:
         lines = f.readlines()
         for idx,line in enumerate(lines):
@@ -14,9 +22,11 @@ def main():
             else:
                 items = line.split()
                 image_path = items[0] + '/coarse_tilt_aligned_face.' + items[2] + '.' + items[1]
+                label = labels[items[3]]
 
-                if
-
+                save_folder = os.path.join(dst_folder,label)
+                os.makedirs(save_folder, exist_ok=True)
+                copyfile(os.path.join(scr_folder,image_path),os.path.join(save_folder, items[0]+'_' +items[2]))
 
 if __name__ == '__main__':
     main()
